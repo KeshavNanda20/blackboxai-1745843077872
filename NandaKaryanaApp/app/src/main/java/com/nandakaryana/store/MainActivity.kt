77acd6828b889cece906e252003cb99208300658
KeshavNanda20 <115359498+KeshavNanda20@users.noreply.github.com>
2025-04-28 17:55:13 +0530

    Add NandaKaryanaApp/app/src/main/java/com/nandakaryana/store/MainActivity.kt 1745843112397

---
+++ NandaKaryanaApp/app/src/main/java/com/nandakaryana/store/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.nandakaryana.store
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.nandakaryana.store.ui.theme.NandaKaryanaTheme
+import com.nandakaryana.store.navigation.NavGraph
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NandaKaryanaTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    NavGraph(navController = navController)
+                }
+            }
+        }
+    }
+}
